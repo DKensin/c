f5b90a898ba94e891d47dc3029de269576277ca1
BHien <hienmit723@gmail.com>
2023-05-04 15:50:53 +0700

    Assignments of pointer

---
+++ exercise/pointer/assignments/pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define MAX_SIZE    100
+
+int main(void)
+{
+    int n;  /* size array */
+    int arr[MAX_SIZE] = {0};
+    int index = 0;
+    int *ptr = arr; /* pointer to array */
+
+    /* input value to array use ptr instead of arr[i] */
+
+    /* print array value use ptr instead of arr[i] */
+
+    return 0;
+}
